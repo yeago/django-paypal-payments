fe55e6790f95a270f9f8c629339a0a18761a63f8
steve yeago <subsume@gmail.com>
2016-08-17 18:43:02 -0400

    some more things in the admin

--- paypal_payments/admin.py
+++ paypal_payments/admin.py
@@ -5,7 +5,7 @@ from paypal_payments.models import RecurringSubscription
 
 class RecurringAdmin(admin.ModelAdmin):
     raw_id_fields = ['user']
-    list_display = ['user', 'last_update', 'profile_status']
+    list_display = ['user', 'last_update', 'profile_status', 'txn_id', 'txn_type']
 
 
 admin.site.register(RecurringSubscription, RecurringAdmin)
